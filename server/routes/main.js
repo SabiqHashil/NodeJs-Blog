01f2de9c99a84df152c105a6ccc68439e7eb8aaa
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-07-09 01:44:44 +0530

    update

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,17 +2,69 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
+router.get("", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb ",
+    };
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// router.get("", async (req, res) => {
+//   const locals = {
+//     title: "NodeJs Blog",
+//     description: "Simple Blog created with NodeJs, Express & MongoDb ",
+//   };
+
+//   try {
+//     const data = await Post.find();
+//     res.render("index", { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
 /**
  * GET
- * HOME
+ * Post :id
  */
-router.get("", (req, res) => {
-  const locals = {
-    title: "NodeJs Blog",
-    description: "Simple Blog created with NodeJs, Express & MongoDb ",
-  };
 
-  res.render("index", { locals });
+router.get("/post/:id", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb ",
+    };
+
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+    res.render("post", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
