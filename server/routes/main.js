facd21d0fde3f083b7d4785e778648979f0c89e4
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-07-12 22:41:26 +0530

    update

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
+/**
+ * GET /
+ * HOME
+ */
+
 router.get("", async (req, res) => {
   try {
     const locals = {
